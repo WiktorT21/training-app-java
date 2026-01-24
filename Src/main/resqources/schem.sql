d16499ee2ce3cc3548fed94b2ddb4016a8db8405
Wiktor <twardzikwiktor3@gmail.com>
2026-01-24 18:15:15 +0100

    Dodanie klasy DatabaseConnection

--- Src/main/resqources/schem.sql
+++ Src/main/resqources/schem.sql
@@ -1,24 +1,24 @@
 -- Tabela dla ćwiczeń w treningach
 CREATE TABLE training_exercises (
-    id SERIAL PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     training_id INTEGER NOT NULL,
     exercise_id INTEGER NOT NULL,
     sets INTEGER NOT NULL,
     reps INTEGER NOT NULL,
-    weight Real NOT NULL,
+    weight REAL NOT NULL,
     FOREIGN KEY (training_id) REFERENCES trainings(id),
     FOREIGN KEY (exercise_id) REFERENCES exercises(id)
 );
 
 -- Tabela dla treningów
 CREATE TABLE trainings (
-    id SERIAL PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     date DATE NOT NULL
 );
 
 -- Tabela dla rodzajów ćwiczeń
 CREATE TABLE exercises (
-    id SERIAL PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(255) NOT NULL,
     muscle_group VARCHAR(255) NOT NULL,
     description TEXT
